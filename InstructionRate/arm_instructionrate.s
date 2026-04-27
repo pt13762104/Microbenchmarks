9a1f4c128ae32bc4a7296a73dd3070e7e7b1dbb3
Yoshi_likes_e4 <104140648+pt13762104@users.noreply.github.com>
2026-04-27 18:47:23 +0700

    3rd test of trying to saturate the core (impossible)

--- InstructionRate/arm_instructionrate.s
+++ InstructionRate/arm_instructionrate.s
@@ -1963,19 +1963,34 @@ mixvecaddmul128test_loop:
 
 _mixaddvecadd128test:
 mixaddvecadd128test:
-  sub sp, sp, #0x50
+  sub sp, sp, #0xa0
   stp x14, x15, [sp, #0x10]
-  stp x12, x13, [sp, #0x20]
-  stp x10, x11, [sp, #0x30]
-  stp x8, x9, [sp, #0x40]
+  stp x16, x17, [sp, #0x20]
+  stp x18, x19, [sp, #0x30]
+  stp x20, x21, [sp, #0x40]
+  stp x22, x23, [sp, #0x50]
+  stp x24, x25, [sp, #0x60]
+  stp x26, x27, [sp, #0x70]
+  stp x28, x29, [sp, #0x80]
+  stp x12, x13, [sp, #0x90]
   mov x15, 1
-  mov x14, 24
-  eor x13, x13, x13
-  eor x12, x12, x12
-  eor x11, x11, x11
-  eor x10, x10, x10
-  eor x9, x9, x9
-  eor x8, x8, x8
+  mov x14, 48
+  eor x16,x16,x16
+  eor x17,x17,x17
+  eor x18,x18,x18
+  eor x19,x19,x19
+  eor x20,x20,x20
+  eor x21,x21,x21
+  eor x22,x22,x22
+  eor x23,x23,x23
+  eor x24,x24,x24
+  eor x25,x25,x25
+  eor x26,x26,x26
+  eor x27,x27,x27
+  eor x28,x28,x28
+  eor x29,x29,x29
+  eor x12,x12,x12
+  eor x13,x13,x13
   ldr q16, [x1]
   ldr q17, [x1]
   ldr q18, [x1]
@@ -1985,37 +2000,66 @@ mixaddvecadd128test:
   ldr q22, [x1]
   ldr q23, [x1]
 mixaddvecadd128test_loop:
-  add x13, x13, x13
-  add x12, x12, x12
+  add x12,x12,x15
+  add x13,x13,x15
   add v16.4s, v16.4s, v16.4s
-  add x11, x11, x11
-  add x10, x10, x10
+  add x16,x16,x15
+  add x17,x17,x15
   add v17.4s, v17.4s, v17.4s
-  add x13, x13, x13
-  add x12, x12, x12
+  add x18,x18,x15
+  add x19,x19,x15
   add v18.4s, v18.4s, v18.4s
-  add x11, x11, x11
-  add x10, x10, x10
+  add x20,x20,x15
+  add x21,x21,x15
   add v19.4s, v19.4s, v19.4s
-  add x13, x13, x13
-  add x12, x12, x12
+  add x22,x22,x15
+  add x23,x23,x15
   add v20.4s, v20.4s, v20.4s
-  add x11, x11, x11
-  add x10, x10, x10
+  add x24,x24,x15
+  add x25,x25,x15
   add v21.4s, v21.4s, v21.4s
-  add x13, x13, x13
-  add x12, x12, x12
+  add x26,x26,x15
+  add x27,x27,x15
   add v22.4s, v22.4s, v22.4s
-  add x11, x11, x11
-  add x10, x10, x10
+  add x28,x28,x15
+  add x29,x29,x15
+  add v23.4s, v23.4s, v23.4s
+  add x12,x12,x15
+  add x13,x13,x15
+  add v16.4s, v16.4s, v16.4s
+  add x16,x16,x15
+  add x17,x17,x15
+  add v17.4s, v17.4s, v17.4s
+  add x18,x18,x15
+  add x19,x19,x15
+  add v18.4s, v18.4s, v18.4s
+  add x20,x20,x15
+  add x21,x21,x15
+  add v19.4s, v19.4s, v19.4s
+  add x22,x22,x15
+  add x23,x23,x15
+  add v20.4s, v20.4s, v20.4s
+  add x24,x24,x15
+  add x25,x25,x15
+  add v21.4s, v21.4s, v21.4s
+  add x26,x26,x15
+  add x27,x27,x15
+  add v22.4s, v22.4s, v22.4s
+  add x28,x28,x15
+  add x29,x29,x15
   add v23.4s, v23.4s, v23.4s
   sub x0, x0, x14
   cbnz x0, mixaddvecadd128test_loop
-  ldp x8, x9, [sp, #0x40]
-  ldp x10, x11, [sp, #0x30]
-  ldp x12, x13, [sp, #0x20]
+  ldp x12, x13, [sp, #0x90]
+  ldp x28, x29, [sp, #0x80]
+  ldp x26, x27, [sp, #0x70]
+  ldp x24, x25, [sp, #0x60]
+  ldp x22, x23, [sp, #0x50]
+  ldp x20, x21, [sp, #0x40]
+  ldp x18, x19, [sp, #0x30]
+  ldp x16, x17, [sp, #0x20]
   ldp x14, x15, [sp, #0x10]
-  add sp, sp, #0x50
+  add sp, sp, #0xa0
   ret
 
 _mix3to1addvecadd128test:
